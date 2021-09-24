4d69cd655c5a9d7a67985319a007ef3cc161e81d
Nandan Bhat <48284547+mynamedoesntfi@users.noreply.github.com>
2021-09-24 19:36:16 +0530

    Updated products module and appmodule

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      // canActivate takes array of all guards to activate
+      {
+        path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent,
+      },
+    ])
+  ],
 })
-export class ProductModule { }
+export class ProductModule {}
